4faa51b15c14a2c285ba7cb57351bf020cd95194
cliff777 <bugman12345@gmail.com>
2015-01-28 12:44:19 -0500

    Spacing

--- src/main/Entity.java
+++ src/main/Entity.java
@@ -3,24 +3,25 @@ package main;
 import java.awt.Graphics2D;
 
 public class Entity {
-	int x,y;
+	int x, y;
 	String name;
+
 	public Entity(String name, int x, int y)
 	{
-		
-		this.x=x;
-		this.y=y;
-		this.name=name;
+
+		this.x = x;
+		this.y = y;
+		this.name = name;
 	}
-	
+
 	public void logic()
 	{
-		
+
 	}
-	
+
 	public void draw(Graphics2D g)
 	{
-		
+
 	}
 
 }
